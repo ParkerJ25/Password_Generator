8e7b1dea97730dc71ec08a3d43e4dda153096e87
Alexey <aleksej.serbinov29@gmail.com>
2022-05-05 20:27:47 +0300

    added a note to a user

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 chars = "abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%^&*()"
 
 while 1:
+    print("Note: recommended minimum password length is 8 characters")
     password_len = int(input("What length would you like your password to be :"))
     password_count = int(input("How many passwords would you like: "))
     for x in range(0,password_count):
