d5a5f4f192037ee6c11fbd54cdddc71a9cde6969
Alexey <aleksej.serbinov29@gmail.com>
2022-05-05 20:37:10 +0300

    code formatted to PEP8 standard

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import random 
+import random
 
 chars = "abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%^&*()"
+
 
 while 1:
     print("Note: recommended minimum password length is 8 characters")
     password_len = int(input("What length would you like your password to be :"))
     password_count = int(input("How many passwords would you like: "))
-    for x in range(0,password_count):
+    for x in range(0, password_count):
         password = ""
         for x in range(0, password_len):
             password_char = random.choice(chars)
-            password      = password + password_char
-        print ("Here is your password: ", password)              
+            password = password + password_char
+        print("Here is your password: ", password)
